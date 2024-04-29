55ade823382ce86069f870e37c27fbf45c2e1109
Sophia Alvarez <101871038+SAlvarezBulfon@users.noreply.github.com>
2024-04-28 22:07:10 -0300

    Aplicación de genéricos

---
+++ src/types/persona.ts
@@ -0,0 +1,14 @@
+import DataModel  from "./DataModel";
+
+/**
+ * Interfaz que define la estructura de un objeto persona.
+ * Contiene propiedades relacionadas con la información personal de una persona.
+ */
+export default interface persona  extends DataModel<persona>{
+  email: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  adress: string;
+  birthdate: string;
+}
